685c48269ec7b51bacb2bd00b9235124f751bac6
AdamJOtto <aotto@ualberta.ca>
2017-04-03 16:20:39 -0600

    Added context switching to main.c

--- Makefile
+++ Makefile
@@ -4,6 +4,6 @@ CFLAGS = -m32
 all: main.c
 	$(CC) $(CFLAGS) $< -o tvm379
 	#./tvm379 16 8 g 100 1000 f
-
+	#To read the bin files type: hexdump -C quicksort-trace.bin | less
 clean:
 	rm -f *~ *.o tvm379
--- main.c
+++ main.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-int ReadTraceFile(char * FileName, int pageSize, int tblEntry, int quantum);
-int checkIfAllFilesAreRead(int fileFlags[], int size);
+int ReadTraceFile(char * FileName, int pageSize, int tblEntry, int quantum, int startByte);
 
 int main(int argc, char *argv[]) {
 
@@ -27,83 +26,50 @@ int main(int argc, char *argv[]) {
     printf("physpages: %d\n", physpages);
     printf("policy: %s\n", policy);
     
-    int fileNotEmpty[argc - 7];
     int fileLineToRead[argc - 7];
-    int i;
-/*
+    int i, j;
+
     //Set all fileNotEmpty to true/1
     for(i = 0; i < argc - 7; i++) {
-         fileNotEmpty[i] = 1;
          fileLineToRead[i] = 0;
     }
-
-    while(checkIfAllFilesAreRead(fileNotEmpty,argc - 7)){
-	    for(i = 7; i < argc; i++) {
-		if(fileNotEmpty[i - 7]){
-		    fileNotEmpty[i - 7] = ReadTraceFile(argv[i], quantum, fileLineToRead[i-7]);
-                    fileLineToRead[i - 7] = fileLineToRead[i-7] + quantum;
-                }
-	    }
+    i = 7;
+    j = 0;
+    while(j < physpages){
+	printf("Pages Read: %d\n",j);
+	ReadTraceFile(argv[i], pgsize, tlbentries, quantum, fileLineToRead[i - 7]);
+	fileLineToRead[i - 7] = fileLineToRead[i-7] + quantum;
+	j += quantum;
+	i += 1;
+	if(i >= argc)
+		i = 7;
     }
-*/	
-	ReadTraceFile(argv[0], pgsize, tlbentries, quantum);
 
     return 0;
 
 }
 
-int checkIfAllFilesAreRead(int fileFlags[], int size)
-{
-	int i;
-	for(i = 0; i < size; i++) {
-		if(fileFlags[i]) {
-			return 1;
-		}
-	}
-	return 0;
-}
-
-int ReadTraceFile(char * FileName, int pageSize, int tblEntry, int quantum) {
+int ReadTraceFile(char * FileName, int pageSize, int tblEntry, int quantum, int startByte) {
 	int i, j;
 	int bytesToRead = (pageSize + tblEntry) / 8;
 	FILE *fp;	
 	fp = fopen(FileName, "rb");
 	unsigned char buffer[bytesToRead];
-	
-	for(j = 0; j< quantum; j++) {
-		
+	for(j = 0; j < quantum + startByte; j++) {
 		if (fread(buffer,bytesToRead,1,fp) == 1) {
-			for(i = 0; i < bytesToRead; i++)
-				printf("%x ", buffer[i]);
-			printf("\n");
-		}
-	}
-	
-	/*
-	for(i = 0; i < quantum + startLine; i++) {
-		if (fscanf(fp, "%s", line) != EOF)
-		{
-			if(i >= startLine) {
-				//This is where we should process the read in lines.
-				printf("%s\n",line);
+			
+			if(j >= startByte) {
+				printf("%d: ", j);
+				for(i = 0; i < bytesToRead; i++) {
+					printf("%x ", buffer[i]);
+				}
+				printf("\n");
 			}
 		}
 		else
-		{
 			return 0;
-		}
+		
 	}
-	*/
 	fclose(fp);
 	return 1;
 }
-
-
-
-
-
-
-
-
-
-
