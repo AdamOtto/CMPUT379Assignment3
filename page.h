4eafa7ae2819a7d7a9212c8efb16fd1f544c2651
Justin <stuehmer@ualberta.ca>
2017-04-11 15:50:55 -0600

    Deleted unused files

--- page.h
+++
@@ -1,7 +0,0 @@
-typedef struct {
-    int PageNumber;       // Page #
-    int FrameNumber;      // Frame #
-    int Valid;            // Valid or invalid bit
-    } page_table_entry;
-
-typedef page_table_entry *page_table;
